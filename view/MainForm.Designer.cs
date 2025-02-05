059db327ad0e8a08831410b71707b5bd401f3c25
ekastn <118880703+ekastn@users.noreply.github.com>
2025-02-05 16:29:11 +0700

    update ui

--- view/MainForm.Designer.cs
+++ view/MainForm.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
             button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panelChild = new Panel();
+            panelTop = new Panel();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -48,54 +50,6 @@
             panel1.Name = "panel1";
             panel1.Size = new Size(190, 563);
             panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            button1.AutoSize = true;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(0, 87);
-            button1.Name = "button1";
-            button1.Padding = new Padding(12, 0, 0, 0);
-            button1.Size = new Size(190, 35);
-            button1.TabIndex = 1;
-            button1.Text = "Dashboard";
-            button1.TextAlign = ContentAlignment.MiddleLeft;
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.AutoSize = true;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(3, 128);
-            button2.Name = "button2";
-            button2.Padding = new Padding(12, 0, 0, 0);
-            button2.Size = new Size(190, 35);
-            button2.TabIndex = 1;
-            button2.Text = "Artists";
-            button2.TextAlign = ContentAlignment.MiddleLeft;
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.AutoSize = true;
-            button3.FlatAppearance.BorderSize = 0;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.White;
-            button3.Location = new Point(3, 169);
-            button3.Name = "button3";
-            button3.Padding = new Padding(12, 0, 0, 0);
-            button3.Size = new Size(190, 35);
-            button3.TabIndex = 1;
-            button3.Text = "Albums";
-            button3.TextAlign = ContentAlignment.MiddleLeft;
-            button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
@@ -113,11 +67,79 @@
             button4.TextAlign = ContentAlignment.MiddleLeft;
             button4.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.AutoSize = true;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(3, 169);
+            button3.Name = "button3";
+            button3.Padding = new Padding(12, 0, 0, 0);
+            button3.Size = new Size(190, 35);
+            button3.TabIndex = 1;
+            button3.Text = "Albums";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(3, 128);
+            button2.Name = "button2";
+            button2.Padding = new Padding(12, 0, 0, 0);
+            button2.Size = new Size(190, 35);
+            button2.TabIndex = 1;
+            button2.Text = "Artists";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(0, 87);
+            button1.Name = "button1";
+            button1.Padding = new Padding(12, 0, 0, 0);
+            button1.Size = new Size(190, 35);
+            button1.TabIndex = 1;
+            button1.Text = "Dashboard";
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panelChild
+            // 
+            panelChild.BackColor = SystemColors.ControlDarkDark;
+            panelChild.Dock = DockStyle.Fill;
+            panelChild.Location = new Point(190, 0);
+            panelChild.Name = "panelChild";
+            panelChild.Size = new Size(733, 563);
+            panelChild.TabIndex = 1;
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = SystemColors.ActiveCaption;
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(190, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(733, 51);
+            panelTop.TabIndex = 2;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(923, 563);
+            Controls.Add(panelTop);
+            Controls.Add(panelChild);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "MainForm";
@@ -134,5 +156,7 @@
         private Button button4;
         private Button button3;
         private Button button2;
+        private Panel panelChild;
+        private Panel panelTop;
     }
 }
